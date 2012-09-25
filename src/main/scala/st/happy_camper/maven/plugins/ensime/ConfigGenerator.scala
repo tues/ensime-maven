5adda2e823ebbf414ce16436e9f5c76fa8a3a808
Takuya UESHIN <ueshin@happy-camper.st>
2012-09-26 04:20:05 +0900

    Add module configurations to .ensime file.

--- src/main/scala/st/happy_camper/maven/plugins/ensime/ConfigGenerator.scala
+++ src/main/scala/st/happy_camper/maven/plugins/ensime/ConfigGenerator.scala
@@ -16,6 +16,7 @@
 package st.happy_camper.maven.plugins.ensime
 
 import java.io.File
+import java.io.FileOutputStream
 import java.util.{ List => JList }
 import java.util.{ Set => JSet }
 
@@ -42,48 +43,58 @@ class ConfigGenerator(
 
   def generate(out: File): Unit = {
 
-    def collectModules(project: MavenProject) = {
-      // TODO how to collect modules?
-      List(project)
+    val modules = (project :: project.getCollectedProjects.asInstanceOf[JList[MavenProject]].toList).filter {
+      project => project.getPackaging != "pom"
     }
+    val artifactToModule = modules.map { module => module.getArtifact -> module }.toMap
 
     implicit val MavenProjectAsSubProject = new As[MavenProject, SubProject] {
 
       def as(project: MavenProject): SubProject = {
-        val (runtimeDeps, compileDeps, testDeps) =
-          (project.getArtifacts.asInstanceOf[JSet[Artifact]] :\ (List.empty[String], List.empty[String], List.empty[String])) {
-            case (artifact, (runtimeDeps, compileDeps, testDeps)) =>
-              val path = artifact.getFile.getAbsolutePath
-              artifact.getScope match {
-                case Artifact.SCOPE_PROVIDED =>
-                  (runtimeDeps, path :: compileDeps, path :: testDeps)
-                case Artifact.SCOPE_RUNTIME =>
-                  (path :: runtimeDeps, compileDeps, path :: testDeps)
-                case Artifact.SCOPE_TEST =>
-                  (runtimeDeps, compileDeps, path :: testDeps)
-                case _ =>
-                  (path :: runtimeDeps, path :: compileDeps, path :: testDeps)
+        val (dependsOnModules, runtimeDeps, compileDeps, testDeps) =
+          (project.getArtifacts.asInstanceOf[JSet[Artifact]] :\ (List.empty[MavenProject], List.empty[String], List.empty[String], List.empty[String])) {
+            case (artifact, (dependsOnModules, runtimeDeps, compileDeps, testDeps)) =>
+              if (artifactToModule.contains(artifact)) {
+                (artifactToModule(artifact) :: dependsOnModules, runtimeDeps, compileDeps, testDeps)
+              } else {
+                val path = artifact.getFile.getAbsolutePath
+                artifact.getScope match {
+                  case Artifact.SCOPE_PROVIDED =>
+                    (dependsOnModules, runtimeDeps, path :: compileDeps, path :: testDeps)
+                  case Artifact.SCOPE_RUNTIME =>
+                    (dependsOnModules, path :: runtimeDeps, compileDeps, path :: testDeps)
+                  case Artifact.SCOPE_TEST =>
+                    (dependsOnModules, runtimeDeps, compileDeps, path :: testDeps)
+                  case _ =>
+                    (dependsOnModules, path :: runtimeDeps, path :: compileDeps, path :: testDeps)
+                }
               }
           }
 
         SubProject(
           project.getArtifactId,
           project.getVersion,
-          runtimeDeps :+ project.getBuild.getOutputDirectory,
-          compileDeps,
-          testDeps :+ project.getBuild.getOutputDirectory :+ project.getBuild.getTestOutputDirectory,
+          runtimeDeps ::: ((project :: dependsOnModules).map { module =>
+            module.getBuild.getOutputDirectory
+          }),
+          compileDeps ::: (dependsOnModules.map { module =>
+            module.getBuild.getOutputDirectory
+          }),
+          testDeps ::: ((project :: dependsOnModules).map { module =>
+            module.getBuild.getOutputDirectory
+          } :+ project.getBuild.getTestOutputDirectory),
           project.getCompileSourceRoots.asInstanceOf[JList[String]]
             ++: project.getTestCompileSourceRoots.asInstanceOf[JList[String]]
             ++: project.getResources.asInstanceOf[JList[Resource]].map(_.getDirectory)
             ++: project.getTestResources.asInstanceOf[JList[Resource]].map(_.getDirectory) ++: Nil,
           project.getBuild.getOutputDirectory,
           project.getBuild.getTestOutputDirectory,
-          List.empty[String],
+          dependsOnModules.map(_.getArtifactId),
           Map.empty)
       }
     }
 
-    val emitter = new SExprEmitter(Project(collectModules(project).map(_.as[SubProject])).as[SExpr])
-    emitter.emit(out.asOutput)
+    val emitter = new SExprEmitter(Project(modules.map(_.as[SubProject])).as[SExpr])
+    emitter.emit(new FileOutputStream(out).asOutput)
   }
 }
